4eafb91874b9b1f8a924ba5e534df8848b736139
AnderThomaz <150291677+AnderThomaz@users.noreply.github.com>
2024-03-04 16:09:43 -0300

    Update cad.php

--- cad.php
+++ cad.php
@@ -1,18 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
-</head>
-<body>
-    <header>
-        <h1>Resultado do Processamento</h1>
-    </header>
-    <main>
-        <?php 
-        ?>
-    </main>
-    
-</body>
-</html>
+<?php 
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassowrd = '';
+    $dbName = 'forcpq';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassowrd,$dbName);
+
+    if($conexao ->connect_errno)
+    {
+        echo 'Erro';
+    }
+
+    else
+    {
+    echo 'Conectado com sucesso';
+
+    }
+    ?>
